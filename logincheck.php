40dea7ae45abff1271a9669534eb2d60155b0f47
SupanthaBappy <75001798+SupanthaBappy@users.noreply.github.com>
2021-03-14 20:19:47 +0600

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -1,17 +1,21 @@
 <?php
-	session_start();
 	if(isset($_POST['submit'])){
-
-		$name 	= $_POST['name'];
-		$password 	= $_POST['password'];
-		if($_COOKIE['name'] == "" || $_COOKIE['password'] == ""){
+		
+		$userFile = fopen("userValidationInfo.json", "r");
+		$userData = fread($userFile, filesize('userValidationInfo.json'));
+		$userInfo = json_decode($userData, true);	
+		$name 	= $userInfo['user'];
+		$password 	= $userInfo['pass'];
+		$email 	= $userInfo['email'];
+		setcookie('name',$name,time()+3600,'/');
+		setcookie('pass',$password,time()+3600,'/');
+		setcookie('email',$email,time()+3600,'/');
+		if($name == "" || $password == ""){
 			echo "Null submission";
 		}else{
-			if($name == $_COOKIE['name'] && $password == $_COOKIE['password']){
-				
-				//$_SESSION['status'] = true;
+			if($name == $_POST['name'] && $password == $_POST['password']){
 				setcookie('status', 'true', time()+5000, '/');
-				header('location: inter.php');
+				header('location: index.php');
 
 			}else{
 				echo "invalid user...";
