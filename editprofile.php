28001ce63f058bcd87f752b76e3e403800eb684d
SupanthaBappy <75001798+SupanthaBappy@users.noreply.github.com>
2021-03-14 20:35:05 +0600

    Update and rename editprofile.html to editprofile.php

---
+++ editprofile.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Edit Profile</title>
+</head>
+<body>
+<table border="1" width="100%" cellspacing="0">
+<form method = "post" action ="">
+
+<tr height="100px" >
+     <td align="left" >
+      
+<td align="right">
+<?php include'high.php';?>
+</td>
+</tr>
+
+
+
+
+
+
+<tr height="200px">
+<td colspan="2" align="center">
+
+<?php include'featurprofile.php';?>
+<br>
+</td>
+
+<td colspan="2" align="center">
+<form method = "post" action ="">
+
+<tr height="100px">
+<td colspan="2"><center>Copyright &copy 2020</center></td>
+
+</form>
+</body>
+</html>
